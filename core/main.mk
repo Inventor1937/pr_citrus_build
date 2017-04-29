6d4ad85bc790b8d9bc243558da266f5d39662246
Simao Gomes Viana <xdevs23@outlook.com>
2017-04-29 14:09:35 +0200

    Completely ultrapower-nuke userdata images
    Change-Id: I725366de70e0fb59e1fd516f8dda3a7b9457d5ba

--- core/main.mk
+++ core/main.mk
@@ -79,7 +79,6 @@ dont_bother_goals := clean clobber dataclean installclean \
     help out \
     snod systemimage-nodeps \
     stnod systemtarball-nodeps \
-    userdataimage-nodeps userdatatarball-nodeps \
     cacheimage-nodeps \
     vendorimage-nodeps \
     systemotherimage-nodeps \
@@ -897,16 +896,6 @@ systemtarball: $(INSTALLED_SYSTEMTARBALL_TARGET)
 .PHONY: boottarball
 boottarball: $(INSTALLED_BOOTTARBALL_TARGET)
 
-.PHONY: userdataimage
-userdataimage: $(INSTALLED_USERDATAIMAGE_TARGET)
-
-ifneq (,$(filter userdataimage, $(MAKECMDGOALS)))
-$(call dist-for-goals, userdataimage, $(BUILT_USERDATAIMAGE_TARGET))
-endif
-
-.PHONY: userdatatarball
-userdatatarball: $(INSTALLED_USERDATATARBALL_TARGET)
-
 .PHONY: cacheimage
 cacheimage: $(INSTALLED_CACHEIMAGE_TARGET)
 
@@ -940,7 +929,6 @@ droidcore: files \
 	systemimage \
 	$(INSTALLED_BOOTIMAGE_TARGET) \
 	$(INSTALLED_RECOVERYIMAGE_TARGET) \
-	$(INSTALLED_USERDATAIMAGE_TARGET) \
 	$(INSTALLED_CACHEIMAGE_TARGET) \
 	$(INSTALLED_VENDORIMAGE_TARGET) \
 	$(INSTALLED_SYSTEMOTHERIMAGE_TARGET) \
